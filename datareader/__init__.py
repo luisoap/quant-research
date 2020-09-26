e33136d17a165c9f6f31b27afb31c1c8fa611896
luisoap <luisoap@outlook.com.br>
2020-09-25 21:41:51 -0300

    databases updated

--- datareader/__init__.py
+++
@@ -1,3 +0,0 @@
-from datareader.data import yahoo_adj_close
-
-__all__ = ['yahoo_adj_close']
--- strategies/__init__.py
+++
@@ -1,4 +0,0 @@
-from strategies.equities import momentum_equities
-from strategies.equities import book_market
-
-__all__ = ['momentum_equities', 'book_market']
